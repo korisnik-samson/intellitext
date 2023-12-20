4025a35a86a24e4de6a96e0858bf18639d09510a
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2023-12-20 13:32:01 +0100

    Resolved Dark Mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
             <div>
                 <div className="relative isolate">
                     <div aria-hidden='true' className="pointer-events-none absolute inset-x-0 -top-40
-                     -z-10 transform-gpu overflow-hidden blur-3xl sm: -top-80">
+                     -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80">
                         <div  style={{
                             clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%,' +
                                 ' 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%,' +
