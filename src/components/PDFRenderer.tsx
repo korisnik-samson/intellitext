4cc95642e385442f70fd9eae5f537bb6f2a48031
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2024-01-01 15:08:27 +0100

    Updated PDFRenderer.tsx

--- src/components/PDFRenderer.tsx
+++ src/components/PDFRenderer.tsx
@@ -1,26 +1,100 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Document, Page, pdfjs } from "react-pdf";
 import { PDFRendererProps } from "@/types";
 
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
-import { Loader2 } from "lucide-react";
+import { ChevronUp, ChevronDown, Loader2, Search } from "lucide-react";
 import { useToast } from "@/components/ui/use-toast";
 import { useResizeDetector } from "react-resize-detector";
-
+import { Button } from "@nextui-org/react";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { cn } from "@/lib/utils";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
 const PDFRenderer = ({ url }: PDFRendererProps) => {
     const { toast } = useToast();
-    const { width, ref } = useResizeDetector()
+    const [numPages, setNumPages] = useState<number>();
+    const [currPage, setCurrPage] = useState<number>(1);
+    const [scale, setScale] = useState<number>(1)
+
+    const CustomPageValidator = z.object({
+        page: z.string().refine((num) => Number(num) > 0 && Number(num) <= numPages!)
+    });
+
+    type TCustomPageValidator = z.infer<typeof CustomPageValidator>
+
+    const { register, handleSubmit,
+        formState: { errors }, setValue } = useForm<TCustomPageValidator>({
+        defaultValues: { page: '1' },
+        resolver: zodResolver(CustomPageValidator)
+    })
+
+    const { width, ref } = useResizeDetector();
+
+    const handlePageSubmit = ({ page }: TCustomPageValidator) => {
+        setCurrPage(Number(page));
+        setValue("page", String(page));
+    }
 
     return (
         <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
             <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
                 <div className="flex items-center gap-1.5">
-                    TopBar
+                    <Button disabled={currPage <= 1} onClick={() => {
+                        setCurrPage((prev) => (prev - 1 > 1 ? prev - 1 : 1))
+                    }} variant="ghost" aria-label='previous page'>
+                        <ChevronDown className="h-4 w-4" />
+                    </Button>
+
+                    <div className="flex items-center gap-1.5">
+                        <Input {...register('page')} className={cn(`w-12 h-8`, errors.page && "focus-visible:ring-red-500")}
+                               onKeyDown={(ev) => {
+                            if (ev.key === "Enter") handleSubmit(handlePageSubmit)()
+                        }} />
+                        <p className="text-zinc-700 text-sm space-x-1">
+                            <span>/</span>
+                            <span>{numPages ?? 'x'}</span>
+                        </p>
+                    </div>
+
+                    <Button disabled={numPages === undefined || currPage === numPages} onClick={() => {
+                        setCurrPage((prev) => (prev + 1 > numPages! ? numPages! - 1 : prev + 1))
+                    }} variant="solid" aria-label='next page'>
+                        <ChevronUp className="h-4 w-4" />
+                    </Button>
+                </div>
+
+                <div className="space-x-2">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button className="gap-1.5 mr-2" aria-label="zoom" variant="ghost">
+                                <Search className="h-5 w-5 mr-2" />
+                                {scale * 100}% <ChevronDown className="h-3 w-3 opacity-50" />
+                            </Button>
+                        </DropdownMenuTrigger>
+
+                        <DropdownMenuContent>
+                            <DropdownMenuItem onSelect={() => setScale(1)}>
+                                100%
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onSelect={() => setScale(1.5)}>
+                                150%
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onSelect={() => setScale(2)}>
+                                200%
+                            </DropdownMenuItem>
+                            <DropdownMenuItem onSelect={() => setScale(2.5)}>
+                                250%
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </div>
             </div>
 
@@ -36,8 +110,9 @@ const PDFRenderer = ({ url }: PDFRendererProps) => {
                             description: 'Please try again later',
                             variant: 'destructive'
                         })
-                    }} file={url} className="max-h-full">
-                        <Page width={width ? width : 1} pageNumber={1} />
+                    }} onLoadSuccess={({ numPages }) => setNumPages(numPages)}
+                        file={url} className="max-h-full">
+                        <Page width={width ? width : 1} pageNumber={currPage} scale={scale} />
                     </Document>
                 </div>
             </div>
