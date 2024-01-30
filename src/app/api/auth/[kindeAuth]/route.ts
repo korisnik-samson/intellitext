8afe48588e824a6093e268ac5a3f4c303ae4c7bc
Samson Offorjindu <126519078+korisnik-samson@users.noreply.github.com>
2024-01-30 10:43:43 +0100

    Update route.ts

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -2,7 +2,10 @@ import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
 
 // @ts-ignore
+/*
 export async function GET(request, { params }){
     const endpoint = params.kindeAuth;
     return handleAuth(request, endpoint);
-}
+}*/
+
+export const GET = handleAuth();
